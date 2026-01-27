99a6368fa0e5c9325643102709c83a5415389e33
Matheus Geiser <50848469+mattatito@users.noreply.github.com>
2026-01-27 03:22:07 +0000

    style: improve code cohesion

--- infra/errors.js
+++ infra/errors.js
@@ -96,13 +96,11 @@ export class NotFoundError extends Error {
   }
 }
 
-
 export class UnauthorizedError extends Error {
   constructor({ message, action }) {
     super(message || "Usuário não autenticado.");
     this.name = "UnauthorizedError";
-    this.action =
-      action || "Faça o login novamente para continuar.";
+    this.action = action || "Faça o login novamente para continuar.";
     this.statusCode = 401;
   }
 
