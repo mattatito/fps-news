139d3217e15dec2e6bb4bf923c6e82a805b270b7
Matheus Geiser <50848469+mattatito@users.noreply.github.com>
2026-01-29 00:49:58 +0000

    feat: create `findOneValidByToken` function in `models/session`

--- models/session.js
+++ models/session.js
@@ -27,8 +27,32 @@ async function create(userId) {
   }
 }
 
+async function findOneValidByToken(sessionToken) {
+  const sessionFound = await runSelectQuery(sessionToken);
+  return sessionFound;
+
+  async function runSelectQuery(sessionToken) {
+    const results = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        sessions
+      WHERE
+        token = $1
+        AND expires_at > NOW()
+      LIMIT 
+        1    
+    ;`,
+      values: [sessionToken],
+    });
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
+  findOneValidByToken,
   EXPIRATION_IN_MILLISECONDS,
 };
 
