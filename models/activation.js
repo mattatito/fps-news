73c65ddc5e1053a68061bd009679eec136fddfda
Matheus Geiser <50848469+mattatito@users.noreply.github.com>
2026-02-12 03:41:49 +0000

    feat:  send activation email after `user` registration

--- models/activation.js
+++ models/activation.js
@@ -1,13 +1,40 @@
+import database from "infra/database";
 import email from "infra/email";
+import webserver from "infra/webserver";
 
-async function sendEmailToUser(user) {
+const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutos
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const newToken = await runInsertQuery(userId, expiresAt);
+  return newToken;
+
+  async function runInsertQuery(userId, expiresAt) {
+    const results = await database.query({
+      text: `
+        INSERT INTO
+          user_activation_tokens (user_id, expires_at)
+        VALUES
+          ($1, $2)
+        RETURNING
+          *
+      ;`,
+      values: [userId, expiresAt],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function sendEmailToUser(user, activationToken) {
   await email.send({
     from: "FpsNews <contato@fpsnews.dev>",
     to: user.email,
     subject: "Ative seu cadastro no FpsNews!",
     text: `${user.username}, clique no link abaixo para ativar seu cadastro no FpsNews:
       
-https://link...
+${webserver.origin}/cadastro/ativar/${activationToken.id}
 
 Atenciosamente,
 Equipe FpsNews.
@@ -15,8 +42,33 @@ Equipe FpsNews.
   });
 }
 
+async function findOneByUserId(userId) {
+  const token = await runSelectQuery(userId);
+  return token;
+
+  async function runSelectQuery(userId) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          user_id = $1
+        LIMIT
+          1
+      ;`,
+      values: [userId],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const activation = {
   sendEmailToUser,
+  create,
+  findOneByUserId,
 };
 
 export default activation;
--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -1,3 +1,4 @@
+import activation from "models/activation";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
@@ -8,6 +9,8 @@ beforeAll(async () => {
 });
 
 describe("Use case: Registration flow (All successful)", () => {
+  let createdUserResponseBody;
+
   test("Create user account", async () => {
     const createdUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -26,7 +29,7 @@ describe("Use case: Registration flow (All successful)", () => {
 
     expect(createdUserResponse.status).toBe(201);
 
-    const createdUserResponseBody = await createdUserResponse.json();
+    createdUserResponseBody = await createdUserResponse.json();
 
     expect(createdUserResponseBody).toEqual({
       id: createdUserResponseBody.id,
@@ -41,11 +44,15 @@ describe("Use case: Registration flow (All successful)", () => {
 
   test("Receive activation email", async () => {
     const lastEmail = await orchestrator.getLastEmail();
+    const activationToken = await activation.findOneByUserId(
+      createdUserResponseBody.id,
+    );
 
     expect(lastEmail.sender).toBe("<contato@fpsnews.dev>");
     expect(lastEmail.recipients[0]).toBe("<registration.flow@curso.dev>");
     expect(lastEmail.subject).toBe("Ative seu cadastro no FpsNews!");
     expect(lastEmail.text).toContain("RegistrationFlow");
+    expect(lastEmail.text).toContain(activationToken.id);
   });
 
   test("Activate account", async () => {});
